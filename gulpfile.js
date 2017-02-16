8cdad50315c847c0886bb0cbfa39c97be8c9645d
duy nguyen <blinkblinklink@aim.com>
2017-02-16 01:38:11 +0000

    Added build-production and build-production-clean to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,46 @@
-var gulp = require('gulp');
-var less = require('gulp-less');
-var browserSync = require('browser-sync').create();
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var minifyHtml = require('gulp-minify-html');
-var gulpIf = require('gulp-if');
-var concat = require('gulp-concat');
-var jsonMinify = require('gulp-jsonminify');
-var pkg = require('./package.json');
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    pkg = require('./package.json'),
+    clean = require('gulp-clean'),
+    header = require('gulp-header'),
+    rename = require("gulp-rename"),
+    uglify = require('gulp-uglify'),
+    gulpIf = require('gulp-if'),
+    concat = require('gulp-concat'),
+    cleanCSS = require('gulp-clean-css'),
+    minifyHtml = require('gulp-minify-html'),
+    jsonMinify = require('gulp-jsonminify'),
+    runSequence = require('run-sequence'),
+    browserSync = require('browser-sync').create(),
+    
 
-var outputDir;
-var env = process.env.NODE_ENV || 'development';
-
-if (env==='development') {
-    outputDir = 'builds/development/';
-} else {
-    outputDir = 'builds/production/';
-}
-
-var devPath = 'builds/development/';
-var jsSrcs = [devPath+'js/new-age.js', devPath+'js/app.js']; 
-var componentHtmlSrcs = [devPath+'components/**/*.html']; 
-var jsonSrcs = [devPath+'components/**/*.json'];
+    env = process.env.NODE_ENV || 'development',
+    devPath = 'builds/development/',
+    prodPath = 'builds/production/',
+    outputDir = setOutputDir(env),
+    jsSrcs = [devPath+'js/new-age.js', devPath+'js/app.js'], 
+    componentHtmlSrcs = [devPath+'components/**/*.html'], 
+    jsonSrcs = [devPath+'components/**/*.json'],
 
 // Set the banner content
-var banner = ['/*!\n',
+    banner = ['/*!\n',
     ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
     ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
     ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
     ' */\n',
     ''
 ].join('');
+
+function setOutputDir(envir) {
+
+    if (envir==='development') {
+        return devPath;
+    } else {
+        return prodPath;
+    }
+}
+
+// ----------------------------------------- TASKS BELOW -----------------------------------------
 
 // Compile LESS files from /less into /css
 gulp.task('less', function() {
@@ -141,6 +149,11 @@ gulp.task('browserSync', function() {
     })
 })
 
+// Clean out production directory
+gulp.task('build-production-clean', function() {
+    return gulp.src(prodPath+"*").pipe(clean());
+});
+
 // Dev task with browserSync
 gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js', 'minify-html', 'images', 'minify-component-html', 'json'], function() {
     gulp.watch(devPath+'less/*.less', ['less']);
@@ -150,4 +163,11 @@ gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js', 'minify-html
     gulp.watch(devPath+'*.html', browserSync.reload);
     gulp.watch(devPath+'components/**/*.html', browserSync.reload);
     gulp.watch(devPath+'js/**/*.js', browserSync.reload);
+});
+
+// Export code to production
+gulp.task('build-production', function() {
+    env = 'production';
+    outputDir = setOutputDir(env);
+    runSequence('build-production-clean','default');
 });
